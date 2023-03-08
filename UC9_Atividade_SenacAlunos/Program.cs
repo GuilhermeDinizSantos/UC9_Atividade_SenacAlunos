32bad9da010eaa8a8603f3da8328c92ab7613f28
GuilhermeDinizSantos <guilherme.dsantos18@GUA0636335W10-1>
2023-03-07 21:14:44 -0300

    criacao do menu principal

--- UC9_Atividade_SenacAlunos/Program.cs
+++ UC9_Atividade_SenacAlunos/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[,] dados = new string[5, 3];
+int uc = 0;
+int c = 0;
+string controle = "a";
+
+while (controle != "b")
+{
+    Console.WriteLine("======================================================================");
+    Console.WriteLine("=================  -> 1: cadastrar informações <-  ===================");
+    Console.WriteLine("=================  -> 2: pesquisar informações <-  ===================");
+    Console.WriteLine("==========================  -> 3: sair <-  ===========================");
+    Console.WriteLine("======================================================================");
+}
